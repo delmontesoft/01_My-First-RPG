03814f3417478d24f63646ccff0841ba74edfb28
Rodrigo Montes <rjmontes@gmail.com>
2020-06-04 00:54:49 -0400

    Video 017 - I'm a real boy (or soldier)!
    - Changed capsule player for a real character
    - Animated character using animator component and blend
    - Calculated speed based on velocity (direction of movement) and adjusted animation in blend accordingly

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform target = null;
 
     void Update()
     {
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    // [SerializeField] Transform target = null;
-
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0)) {
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -24,5 +25,13 @@ public class Mover : MonoBehaviour
         if (hasHit) {
             GetComponent<NavMeshAgent>().SetDestination(raycastHit.point);
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
