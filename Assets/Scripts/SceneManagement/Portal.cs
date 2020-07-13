ae54a95eb4a1bb66995fb163be3fab9777d230f8
Rodrigo Montes <rjmontes@gmail.com>
2020-07-13 14:14:46 -0400

    Video 074 - Portal, portals... and a corridor between them
    - Make the Level load asynchronous when crossing a portal
    - Added spawn point to the portals
    - Spawn the player on the spawn point position and direction when entering the new scene
    - Added an identifier to match pairs of portals on different levels (in/out portals)

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E, F, G, H, I, J, K, L, M, N
+        }
+
+        [SerializeField] DestinationIdentifier destinationPortalID;
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint = null;
 
         private void OnTriggerEnter(Collider other) 
         {
@@ -17,9 +27,47 @@ namespace RPG.SceneManagement
 
         private void LoadScene()
         {
-            if (sceneToLoad < 0 || sceneToLoad >= SceneManager.sceneCountInBuildSettings) return;
+            if (sceneToLoad < 0 || sceneToLoad >= SceneManager.sceneCountInBuildSettings) 
+            {
+                Debug.LogAssertion(this.name + " has no destination scene to load set!");
+                return;
+            }
+            
 
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(Transition());
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(this.gameObject);
+
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            UpdatePlayer(GetDestinationPortal());
+
+            Destroy(this.gameObject);
+        }
+
+        private Portal GetDestinationPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destinationPortalID != destinationPortalID) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+
         }
     }
 }
